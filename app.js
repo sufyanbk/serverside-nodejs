b36fd2c4d16c66d5da935eef129a6378ed8078c5
Your Name <you@example.com>
2024-08-21 12:59:41 +0000

    fixed merge

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 const express = require('express');
-const sequelize = require('./config/database');
-const portfolioRoutes = require('./routes/portfolioRoutes');
-//const assetRoutes = require('./routes/assetsRoutes');
-//const transactionRoutes = require('./routes/transactionsRoutes');
-
 const app = express();
 const assetRoutes = require('./routes/assetRoutes');
 const transactionRoutes = require('./routes/transactionRoutes');
 const sequelize = require('./config/database');
 const portfolioRoutes = require('./routes/portfolioRoutes');
-//const assetRoutes = require('./routes/assetsRoutes');
-//const transactionRoutes = require('./routes/transactionsRoutes');
 
 // Middleware to parse JSON bodies
 app.use(express.json());
