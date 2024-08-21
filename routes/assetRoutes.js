dc36270721a5fef3fee4e8b3faf1ccea5f79826a
Sufyan Khurram <sufyankhurram2000@gmail.com>
2024-08-21 11:13:35 +0100

    Fixing the route issue

--- routes/assetRoutes.js
+++ routes/assetRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 // Import the controller that handles asset-related logic
-const assetController = require('../models/assetController');
+const assetController = require('../controllers/assetController');
 
 // Define the route for GET /assets
 // This route will call the getAllAssets controller function to fetch all assets
