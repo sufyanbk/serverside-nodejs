944eec8957f26bcbd3adb5d7c78ba6107cf4bf99
sam.cooke793 <sam.cooke793@gmail.com>
2024-08-21 10:43:55 +0000

    Updated transaction model in models

---
+++ models/transaction.js
@@ -0,0 +1,37 @@
+const { DataTypes } = require('sequelize');
+
+// Import the configured sequelize instance
+const sequelize = require('../config/database');
+
+const Transaction = sequelize.define('Transaction', {
+    asset_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      asset_type: {
+        type: DataTypes.ENUM('buy', 'sell'),
+        allowNull: false,
+      },
+      quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+      },
+      transaction_date: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      }
+    }, {
+      timestamps: true,
+      tableName: 'transactions',
+    });
+  
+module.exports = Transaction;
+  
